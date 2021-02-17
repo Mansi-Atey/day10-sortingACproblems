81315b69b09299531ea9f7886c1cd108f0a8195e
unknown <you@example.com>
2021-02-18 01:22:57 +0530

    Added: values in dictionary

--- day10sort.sh
+++ day10sort.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+declare -A associative
+associative[key1]=T1
+associative[key2]=T2
+associative[key3]=T3
+associative[key4]=T4
 read -p "Enter First Number." a
 read -p "Enter Second Number." b
 read -p  "Enter Third Number." c
-T4=$(($(($a % $a)) + $b))
-echo $T4
+T1=$(($(($a + $b)) * $c))
+T2=$(($(($a * $b)) + $c))
+T3=$(($(($c + $a)) / $b))
+T4=$(($(($a % $b)) + $c))
+echo ${associative[key1]} $T1
+echo ${associative[key2]} $T2
+echo ${associative[key3]} $T3
+echo ${associative[key4]} $T4
 
 
