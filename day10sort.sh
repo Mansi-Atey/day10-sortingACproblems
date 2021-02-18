8667d02d60b0bf075cb238f793e106cc6c5a3932
Mansi-Atey <78947536+Mansi-Atey@users.noreply.github.com>
2021-02-18 09:31:56 +0530

    Update day10sort.sh

--- day10sort.sh
+++ day10sort.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-declare -A associative
-associative[key1]=T1
-associative[key2]=T2
-associative[key3]=T3
-associative[key4]=T4
 read -p "Enter First Number." a
 read -p "Enter Second Number." b
 read -p  "Enter Third Number." c
@@ -11,9 +6,14 @@ T1=$(($(($a + $b)) * $c))
 T2=$(($(($a * $b)) + $c))
 T3=$(($(($c + $a)) / $b))
 T4=$(($(($a % $b)) + $c))
-echo ${associative[key1]} $T1
-echo ${associative[key2]} $T2
-echo ${associative[key3]} $T3
-echo ${associative[key4]} $T4
+echo "result for key1=" $T1
+echo "result for key2=" $T2
+echo "result for key3=" $T3
+echo "result for key4=" $T4
 
-
+declare -A record
+record[key1]=$T1
+record[key2]=$T2
+record[key3]=$T3
+record[key4]=$T4
+echo ${record[@]}
